7ac82983c7fe90a3b4e9ea7d14f0ec7a1f0de191
daffanizwar <daffanizwar101@gmail.com>
2021-04-13 12:10:41 +0700

    Tugas

---
+++ Karyawan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3318
+{
+    public class Karyawan
+    {
+        public int identitas;
+        public string nama;
+        public int gaji;
+        public Karyawan(int nik, string name, int gajibulanan)
+
+        {
+
+
+            identitas = nik;
+            nama = name;
+            gaji = gajibulanan;
+
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3318
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NO" + " NIK/NAMA" + "         " + "Gaji Bulanan");
+            Karyawan karyawan1 = new Karyawan(20113318, "Daffa", 3000000);
+            Karyawan karyawan2 = new Karyawan(20113388, "Nizwar", 2000000);
+            Console.WriteLine("1." + karyawan1.identitas + " " + karyawan1.nama + "     " + karyawan1.gaji);
+            Console.WriteLine("2." + karyawan2.identitas + " " + karyawan2.nama + "    " + karyawan2.gaji);
+            if (karyawan1.gaji <= 0 || karyawan2.gaji <= 0)
+            {
+                Console.WriteLine("Anda memasukkan gaji minus");
+                karyawan1.gaji = 0;
+                karyawan2.gaji = 0;
+                Console.WriteLine("1." + karyawan1.identitas + " " + karyawan1.nama + "     " + karyawan1.gaji);
+                Console.WriteLine("2." + karyawan2.identitas + " " + karyawan2.nama + "    " + karyawan2.gaji);
+            }
+            else
+            {
+                Console.WriteLine("Alhamdulillah dapat kenaikan gaji 10%!!");
+                int tambah = karyawan1.gaji * 10 / 100;
+                karyawan1.gaji += tambah;
+                int tambah1 = karyawan2.gaji * 10 / 100;
+                karyawan2.gaji += tambah1;
+                Console.WriteLine("1." + karyawan1.identitas + " " + karyawan1.nama + "     " + karyawan1.gaji);
+                Console.WriteLine("2." + karyawan2.identitas + " " + karyawan2.nama + "    " + karyawan2.gaji);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
